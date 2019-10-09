8025fcfd83987944d8e4712db81f488b831c72c1
sayaktjawsd <inagaki.s.ae@m.titech.ac.jp>
2019-10-09 22:24:52 +0900

    行列生成部分修正

--- encoding_ldpc.py
+++ encoding_ldpc.py
@@ -1,20 +1,35 @@
 import random
-x=[[0]*3]*200
+import os
+x=[[0,0,0] for i in range(200)]
 y_para=[0]*100
+#count=[0]*100 #確認用
+
+#行列の生成
 
 for num in range(200):
+    m=[-1,-1,-1]
     for n in range(3):
         x[num][n]=random.randint(0,99)
-        if y_para[x[num][n]]==6:
-            while y_para[x[num][n]]==6:
+        if y_para[x[num][n]]==6 or x[num][n]==m[1] or x[num][n]==m[2]:
+            while y_para[x[num][n]]==6 or x[num][n]==m[1] or x[num][n]==m[2]:
                 x[num][n]=random.randint(0,99)
         y_para[x[num][n]]+=1
-        print(x[num][n])
+        m[n]=x[num][n]
+'''
+for m in x:
+    for n in m:
+        count[n]+=1
+print(count)
 
-print('/n')
-
-
-
-    
-
+file = os.path.abspath("test.txt")
+with open(file, mode='w') as fp:
+    for n in x:
+        for m in n:
+            fp.write(str(m))
+            fp.write(' ')
+'''
+x=[[0]*200 for i in range(100)]
+for s in x:
+    for t in s:
+        
 
